3995ff3f974f7b809ca7bd29a5d8c3beaa4c82a9
ankingcodes <ankush.bhardwaj0@gmail.com>
2020-03-31 07:25:45 +0530

    added assembly file

---
+++ 16bitadd.s
@@ -0,0 +1,18 @@
+ # Add two 16 bit numbers 
+  TTL 16bitadd
+  AREA program, CODE, READONLY
+  ENTRY
+
+Main
+  LDR R1, Value1
+  LDR R2, Value2
+  ADD R1, R1, R2
+  STR R1, Result
+  SWI &11
+
+Value1 DCW &C123
+       ALIGN
+Value2 DCW &02AA
+       ALIGN
+Result DCW 0
+       END
